70b8f72587638ea94cc3600f70adc8426f1986fc
Timur Olzhabaev <timur.olzhabaev@uni-hamburg.de>
2025-01-25 12:23:05 +0100

    Fix accidentally introduced wrong database path handling.

--- src/lipidome_projector/database/in_memory_df_db.py
+++ src/lipidome_projector/database/in_memory_df_db.py
@@ -4,6 +4,7 @@ import logging
 
 from collections import defaultdict
 from dataclasses import dataclass, field
+from importlib.resources import files
 from pathlib import Path
 from typing import Self
 
@@ -35,11 +36,21 @@ class InMemoryDataFrameDB(BaseDB):
         :returns: The database.
         """
         return cls.from_files(
-            name=config_dict["name"],
-            db_path=Path(config_dict["db_path"]),
-            vectors_2d_path=Path(config_dict["vectors_2d_path"]),
-            vectors_3d_path=Path(config_dict["vectors_3d_path"]),
-            smiles_path=Path(config_dict["smiles_path"]),
+            name=files("lipidome_projector.data").joinpath(
+                config_dict["name"]
+            ),
+            db_path=files("lipidome_projector.data").joinpath(
+                config_dict["db_path"]
+            ),
+            vectors_2d_path=files("lipidome_projector.data").joinpath(
+                config_dict["vectors_2d_path"]
+            ),
+            vectors_3d_path=files("lipidome_projector.data").joinpath(
+                config_dict["vectors_3d_path"]
+            ),
+            smiles_path=files("lipidome_projector.data").joinpath(
+                config_dict["smiles_path"]
+            ),
         )
 
     @classmethod
